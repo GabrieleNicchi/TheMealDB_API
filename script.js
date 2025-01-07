35e12cd3cf48337a5845c9ac7af1254fcd17666f
ExalChini02 <gabriele.nicchi@studenti.unimi.it>
2025-01-07 21:56:03 +0100

    Change-02

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
+
+/* ------------------------------ URL ------------------------------ */
+
 // Funzione per ottenere i parametri dalla URL
-function getQueryParam(param) {
+const getQueryParam = (param) => {
     const urlParams = new URLSearchParams(window.location.search);
     //console.log(String(urlParams));
     return urlParams.get(param);
 }
 
-function getQueryParamNames() {
+// Funzione per ottenere i nomi dei parametri dalla URL
+const getQueryParamNames = () => {
     const params = new URLSearchParams(window.location.search);
     let queryParams = [];
     for (let param of params.keys()) {
@@ -15,8 +19,34 @@ function getQueryParamNames() {
     return String(queryParams);
 }
 
+/* ------------------------------ ELENCO MEAL ------------------------------ */
+
+// Funzione generale per ottenere l'elenco dei pasti
+const getMealList = ( ricerca , mealContainer , type, ref) => {
+
+    fetch('https://www.themealdb.com/api/json/v1/1/' + type + '.php?' + ref + '=' + ricerca)
+    .then(response => response.json())
+    .then(data => {
+    
+    
+    // Cancella il contenuto precedente una sola volta, prima del ciclo
+    mealContainer.innerHTML = '';
+
+    // Se ci sono pasti trovati
+    if (data.meals) {
+        
+        printMeal(data.meals , mealContainer);
+        
+    } else {
+        // Se nessun pasto viene trovato
+        mealContainer.innerHTML = '<p>No meal found.</p>';
+    }
+    }).catch(error => console.error('Error fetching meal data:', error));
+
+}
+
 // Funzione di stampa dei pasti
-function printMeal(data_meal , mealContainer){
+const printMeal = (data_meal , mealContainer) => {
    
     for (var i = 0; i < data_meal.length; i++) {
         const meal = data_meal[i];
@@ -26,10 +56,11 @@ function printMeal(data_meal , mealContainer){
         mealElement.classList.add('meal');
         
         // Inserisci il div nella pagina
+        // Quando l'utente clicca su 'Dettagli' passa l'id del piatto selezionato
         mealElement.innerHTML = `
             <h2>${meal.strMeal}</h2>
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-            <p><a href="dettagli_meal.html">Dettagli</a></p>
+            <p><a href="dettagli_meal.html?idMeal=${meal.idMeal}">Dettagli</a></p>
         `;
         
         // Aggiungi ogni pasto al contenitore
@@ -37,18 +68,63 @@ function printMeal(data_meal , mealContainer){
     }
 }
 
-//funzione per stampare tutti gli ingredienti data una ricetta
-function append_ingredients(meal) {
-    var div_ingredients = document.getElementById('ingredients');
-    div_ingredients.innerHTML = "<p><strong>Ingredients:</strong></p><ul>";
-    for (i = 1; i <= 20; i++) {
-        var ingredient = meal["strIngredient" + i];
-        var measure = meal["strMeasure" + i];
+/* ------------------------------ MEAL DETTAGLI ------------------------------ */
+
+// Funzione generale per ottenere i dettagli di un pasto
+const getDetailMealList = ( id ) => {
+
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+    .then(response => response.json())
+    .then(data => {
+    
+    const mealContainer = document.getElementById('meal-container')
+    
+    // Cancella il contenuto precedente una sola volta, prima del ciclo
+    mealContainer.innerHTML = '';
+
+    // Se c'è un pasto trovato
+    if (data.meals) {
+        
+        printDetailMeal(data.meals[0] , mealContainer);
+        
+    } else {
+        // Errore nella richiesta
+    }
+    }).catch(error => console.error('Error fetching meal data:', error));
+}
+
+// Funzione di stampa di un pasto
+const printDetailMeal = (meal, mealContainer) => {
+    // Crea l'elemento per ogni pasto
+    const mealElement = document.createElement('div');
+    mealElement.classList.add('meal');
+
+    // Inserisci il div nella pagina
+    mealElement.innerHTML = `
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        <p><h3>Categoria:</h3> ${meal.strCategory}</p>
+        <p><h3>Area:</h3> ${meal.strArea}</p>
+        <p><h3>Istruzioni:</h3> ${meal.strInstructions}</p>
+        <div id="ingredients">${append_ingredients(meal)}</div>
+    `;
+
+    // Aggiungi ogni pasto al contenitore
+    mealContainer.appendChild(mealElement);
+}
+
+// Funzione per stampare tutti gli ingredienti dato un pasto
+const append_ingredients = (meal) => {
+    let ingredientsHTML = "<p><strong>Ingredients:</strong></p><ul>";
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal["strIngredient" + i];
+        const measure = meal["strMeasure" + i];
         if (ingredient && ingredient.trim() !== "") {
-            // Creo un link che mi porta alla pagina ingredients2.html che si occupa di mostrare i piatti in base a un ingrediente principale
-            div_ingredients.innerHTML += "<li><a href='Ingredients2.html?ingredient=" + encodeURIComponent(ingredient) + "'>" + ingredient + "</a>" + (measure ? " - " + measure : "") + "</li>";
+             // Creo un link che mi riporta alla pagina 'dettagli_meal.html' che si occupa di mostrare i pasti in base a un ingrediente principale
+            ingredientsHTML += `<li><a href='elenco_meal.html?ingredient=${encodeURIComponent(ingredient)}'>${ingredient}</a>${measure ? " - " + measure : ""}</li>`;
         }
     }
-    div_ingredients.innerHTML += "</ul>";
+    ingredientsHTML += "</ul>";
+    return ingredientsHTML;
 }
 
